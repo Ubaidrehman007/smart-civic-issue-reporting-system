e6c31d493b7b9ef2a87fd390000976c0f182ef70
Ubaid Rehman <urkhan556@gmail.com>
2026-07-27 00:20:22 +0530

    add user repository and registration request validation

---
+++ backend/src/main/java/com/smartcivic/backend/user/dto/RegisterUserRequest.java
@@ -0,0 +1,32 @@
+package com.smartcivic.backend.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record RegisterUserRequest(
+        @NotBlank(message = "Full name is required")
+        @Size(min = 2, max = 100, message = "Full name must be between 2 and 100 charecters")
+        String fullName,
+
+        @NotBlank(message = "Email is required")
+        @Size(max = 255, message = "Email must not exceed 255 charecters")
+        @Email(message = "Email address is invalid")
+        String email,
+
+        @NotBlank(message = "PhoneNumber is Required")
+        @Pattern(
+                regexp = "^[6-9][0-9]{9}$",
+                message = "hone number must be a valid 10-digit Indian mobile number"
+        )
+        String phoneNumber,
+
+        @NotBlank(message = "Password os required")
+        @Size(min = 8, max = 72, message = "Password must be between 8 and 72 Charecters")
+        String password
+        )
+{
+
+
+}
